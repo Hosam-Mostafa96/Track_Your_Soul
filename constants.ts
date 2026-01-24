7e797b029a5b99d4eabe1449ee795536c2a93031
hmis <hosammostafa1996@gmail.com>
2026-01-24 08:17:25 +0200

    fix: Update Google Stats API endpoint
    The API endpoint for synchronizing Google Stats has been updated to a new URL. This change ensures that the application can continue to reliably send and receive data from the Google Sheets backend.

--- constants.ts
+++ constants.ts
@@ -2,7 +2,7 @@
 import { SurroundingSunnah, AppWeights } from './types';
 
 // الرابط الموحد للمزامنة والمحراب العالمي
-export const GOOGLE_STATS_API = "https://script.google.com/macros/s/AKfycbxXTYoO7FpVvGSNWJu2ruMZO3ksc6WS4XujNIBH5X70B9QZvqn3uGoLrhKz3P8j30nZEQ/exec";
+export const GOOGLE_STATS_API = "https://script.google.com/macros/s/AKfycbw-WAB51rCIdtCCqrd8PbqTOEZ_z_KMbTrdyeEIF_pwfS4osD_bLm_d7vytB8MNkkUfnA/exec";
 
 export const DEFAULT_WEIGHTS: AppWeights = {
   fardCongregation: 2700,
